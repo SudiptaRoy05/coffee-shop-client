551aee71ce93c1da14b0ffa24fd5465036d5acc4
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-28 11:57:12 +0600

    invoke form data and making a object named coffee

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -1,9 +1,26 @@
 export default function AddCoffee() {
+  const handleForm = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+
+    const name = form.get("name");
+    const available = form.get("available");
+    const quantity = form.get("quantity");
+    const details = form.get("details");
+    const category = form.get("category");
+    const photoURL = form.get("photoURL");
+
+    const coffee = { name, available, quantity, details, category, photoURL };
+    console.log(coffee);
+  };
+
   return (
     <div className="flex justify-center items-center mt-60 flex-col w-8/12 mx-auto">
-      <h1 className="text-center text-3xl font-bold text-green-500">Add a Coffee</h1>
+      <h1 className="text-center text-3xl font-bold text-green-500">
+        Add a Coffee
+      </h1>
       <div className="card w-full shrink-0 shadow-2xl bg-blue-950">
-        <form className="card-body grid grid-cols-2">
+        <form onSubmit={handleForm} className="card-body grid grid-cols-2">
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
